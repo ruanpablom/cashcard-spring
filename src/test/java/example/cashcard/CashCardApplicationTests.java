418abe82985d76d737f1f3e903ed068a98db6f7a
Ruan Medeiros <pm.ruan@gmail.com>
2025-09-07 19:37:28 -0300

    refactor: make shouldNotReturnCashCardWithUnknowId test pass modifing CashCardController WIP

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -32,4 +32,12 @@ class CashCardApplicationTests {
         assertThat(amount).isEqualTo(123.45);
     }
 
+    @Test
+    void shouldNotReturnACashCardWithAnUnkownId(){
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+        assertThat(response.getBody()).isBlank();
+    }
+
 }
