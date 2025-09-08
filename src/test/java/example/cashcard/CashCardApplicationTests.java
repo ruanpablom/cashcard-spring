1d7ae1b71f7e5093d640d03a0c0816159dffb6a9
Ruan Medeiros <pm.ruan@gmail.com>
2025-09-07 21:18:27 -0300

    refactor: CashCardApplicationsTests add shouldCreateANewCashCard test WIP

--- src/test/java/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/example/cashcard/CashCardApplicationTests.java
@@ -1,14 +1,13 @@
 package example.cashcard;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,13 +17,11 @@ class CashCardApplicationTests {
     TestRestTemplate restTemplate;
 
     @Test
-    void shouldReturnCashCardWhenDataIsSaved(){
+    void shouldReturnACashCardWhenDataIsSaved() {
         ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
-
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
         DocumentContext documentContext = JsonPath.parse(response.getBody());
-
         Number id = documentContext.read("$.id");
         assertThat(id).isEqualTo(99);
 
@@ -33,11 +30,18 @@ class CashCardApplicationTests {
     }
 
     @Test
-    void shouldNotReturnACashCardWithAnUnkownId(){
+    void shouldNotReturnACashCardWithAnUnknownId() {
         ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
 
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
         assertThat(response.getBody()).isBlank();
     }
 
+    @Test
+    void shouldCreateANewCashCard() {
+        CashCard newCashCard = new CashCard(null, 250.00);
+
+        ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
 }
