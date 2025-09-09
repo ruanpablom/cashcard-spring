95e695a5f97c71b5c96480e8f83548f859255e8b
Ruan Medeiros <pm.ruan@gmail.com>
2025-09-09 18:32:37 -0300

    refactor: putCashCard method updated to respond notFound if cashCard is null

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -57,8 +57,11 @@ class CashCardController {
     @PutMapping("/{requestedId}")
     private ResponseEntity<Void> putCashCard(@PathVariable Long requestedId, @RequestBody CashCard cashCardUpdate, Principal principal) {
         CashCard cashCard = cashCardRepository.findByIdAndOwner(requestedId, principal.getName());
-        CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
-        cashCardRepository.save(updatedCashCard);
-        return ResponseEntity.noContent().build();
+        if (cashCard != null) {
+            CashCard updatedCashCard = new CashCard(cashCard.id(), cashCardUpdate.amount(), principal.getName());
+            cashCardRepository.save(updatedCashCard);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
